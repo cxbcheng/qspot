af250bf794f61373eda276fe461afc4a5ca1d3d3
cxbcheng <christian.b.cheng@gmail.com>
2026-06-18 04:48:54 -0400

    refined classical shuffle for high accuracy work grouping

--- shared/utils/shuffle.ts
+++ shared/utils/shuffle.ts
@@ -1,5 +1,10 @@
 import {PlaylistItem} from "../types/Playlist.ts";
 
+// Regexes for track names
+const opusRegex = /\bOp\.?\s*(\d+)/i;
+const noRegex = /\bNo\.?\s*(\d+)/i;
+const catalogRegex = /\b((?:BWV|HWV|RV|KV?|D|S|WWV|Op|Hob)\.?)\s+([IVX0-9]+[a-z]?(?::[0-9]+)?(?:,?\s*No\.?\s*[0-9]+)?)\b/;
+
 /**
  * A list of identifiers used to separate partitions from one another.
  * Each Partition also keeps an array of all the items in it.
@@ -8,10 +13,25 @@ import {PlaylistItem} from "../types/Playlist.ts";
  * The goal is for these partitions to represent a whole work/piece.
  */
 interface PlaylistPartition {
+    discriminators: PartitionDiscriminators;
+    items: PlaylistItem[];
+}
+
+interface PartitionDiscriminators {
     albumId: string;
     primaryArtistName: string;
     trackNumber: number;
-    items: PlaylistItem[];
+    name: string;
+}
+
+interface ClassicalHeuristics {
+    composer: string;
+    opus?: string;
+    no?: string;
+    catalogPrefix?: string;
+    catalogNumber?: string;
+    albumId: string;
+    workTitle?: string;
 }
 
 /**
@@ -57,13 +77,16 @@ export function classicalPartition(tracks: PlaylistItem[]): PlaylistPartition[] 
     const partitions: PlaylistPartition[] = [];
 
     for (const track of tracks) {
-        const albumId: string = track.item.album.id;
-        const primaryArtistName: string = track.item.artists[0].name;
-        const trackNumber: number = track.item.track_number;
+        const trackDiscriminators: PartitionDiscriminators = {
+            albumId: track.item.album.id,
+            primaryArtistName: track.item.artists[0].name,
+            trackNumber: track.item.track_number,
+            name: track.item.name,
+        };
         let foundPartition: boolean = false;
 
         for (const p of partitions) {
-            if (p.albumId === albumId && p.primaryArtistName === primaryArtistName) {
+            if (classicallyEquivalent(trackDiscriminators, p.discriminators)) {
                 p.items.push(track);
                 foundPartition = true;
                 break;
@@ -72,13 +95,45 @@ export function classicalPartition(tracks: PlaylistItem[]): PlaylistPartition[] 
 
         if (!foundPartition) {
             partitions.push({
-                albumId,
-                primaryArtistName,
-                trackNumber,
+                discriminators: trackDiscriminators,
                 items: [track],
             });
         }
     }
 
     return partitions;
+}
+
+function classicallyEquivalent(a: PartitionDiscriminators, b: PartitionDiscriminators): boolean {
+    const getClassicalHeuristics = (track: PartitionDiscriminators): ClassicalHeuristics => {
+        const nameSplits = track.name.split(":");
+        const catalogRegexMatches = track.name.match(catalogRegex);
+
+        return {
+            albumId: track.albumId,
+            opus: track.name.match(opusRegex)?.[1],
+            no: track.name.match(noRegex)?.[1],
+            catalogPrefix: catalogRegexMatches?.[1],
+            catalogNumber: catalogRegexMatches?.[2],
+            composer: track.primaryArtistName,
+            workTitle: nameSplits.length > 1 ? nameSplits[0] : undefined,
+        };
+    };
+
+    const chA = getClassicalHeuristics(a);
+    const chB = getClassicalHeuristics(b);
+
+    if (chA.composer !== chB.composer) return false;
+    if (chA.opus && chB.opus && chA.opus !== chB.opus) return false;
+    if (chA.no && chB.no && chA.no !== chB.no) return false;
+    if (chA.catalogPrefix && chB.catalogPrefix && chA.catalogPrefix !== chB.catalogPrefix) return false;
+    if (chA.catalogNumber && chB.catalogNumber && chA.catalogNumber !== chB.catalogNumber) return false;
+
+    if (chA.workTitle && chB.workTitle) {
+        return chA.workTitle === chB.workTitle;
+    }
+
+    // Fallback: the composer is the same, but we have lots of missing metadata
+    // e.g. tracks with movement-only titles
+    return chA.albumId === chB.albumId;
 }
